8453203f561ca301cc61401f4c0e354f347ee6c2
hochkofler <hochkofler94@gmail.com>
2024-03-11 13:40:55 -0400

    Remove trash files

--- Models/FaseMovil.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebColumnas.Models
-{
-    public class FaseMovil
-    {
-        public int FaseMovilID { get; set; }
-        public required string Nombre { get; set; }
-        public List<Columna>? Columnas{ get; set; }
-    }
-}
--- Models/Proveedor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebColumnas.Models
-{
-    public class Proveedor
-    {
-        public int Id { get; set; }
-        public required string Nombre { get; set; }
-        public ICollection<Marca> Marcas { get; } = new List<Marca>();
-    }
-}
